fe5575191573e2bdc1a560f74510ff6530681f78
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-04 11:21:29 +0900

    add: seedファイルにデータ生成の流れ記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "csv"
+
+CSV.foreach('db/csv/events.csv', headers: true) do |row|
+  Event.find_or_create_by!(name: row['name'], date: row['date']) do |event|
+    event.category = row['category']
+    event.name = row['name']
+    event.name_ruby = row['name_ruby']
+    event.date = row['date']
+    event.place = row['place']
+    event.place_prefecture = row['place_prefecture']
+    event.is_canceled = row['is_canceled']
+  end
+end
