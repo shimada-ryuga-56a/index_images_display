68940c19842a47ef46458d60b2814f880c6566b8
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-04 14:23:20 +0900

    feat: 開発環境初期データ投入用コード記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,11 @@ CSV.foreach('db/csv/events.csv', headers: true) do |row|
     event.is_canceled = row['is_canceled']
   end
 end
+
+CSV.foreach('db/csv/events.csv', headers: true) do |row|
+  event = Event.find_by(name: row['name'], date: row['date'])
+  # if event.visual_image.attached?
+  #   return
+  # end
+  event.visual_image.attach(io: File.open(Rails.root.join(row['visual_image'])),filename: File.basename(row['visual_image']))
+end
